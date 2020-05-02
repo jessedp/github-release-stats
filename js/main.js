da9a36a719f39ecb0d3c9daf95ac767b4039f4eb
jesse <jessedp@gmail.com>
2020-05-02 11:03:19 -0400

    project name. search toggle. ignored assets. file icons

--- js/main.js
+++ js/main.js
@@ -1,203 +1,352 @@
-var apiRoot = "https://api.github.com/";
+const apiRoot = "https://api.github.com/";
 
-function getQueryVariable(variable) {
-    var query = window.location.search.substring(1);
-    var vars = query.split("&");
-    for(var i = 0; i < vars.length; i++) {
-        var pair = vars[i].split("=");
-        if(pair[0] == variable) {
-            return pair[1];
-        }
+// asset filename patterns to ignore in d/l counts
+const ignores = [/yml/, /blockmap/];
+
+// add icons by mime-type (see repo.assests.content_type)
+const iconClassesByMime = {
+  "mime/type": "fa? fa-proper-icon-class",
+};
+
+// add icons by file name (extension)
+const iconClassesByExt = {
+  appimage: "fab fa-linux",
+  yml: "far fa-file-code",
+  exe: "fab fa-windows",
+  msi: "fab fa-windows",
+  dmg: "fab fa-apple",
+  rpm: "fab fa-redhat",
+  deb: "fab fa-ubuntu",
+};
+
+$(document).ready(() => {
+  $("#username").focus();
+});
+
+const shouldIgnore = (str) => {
+  let ignore = false;
+  ignores.forEach((pat) => {
+    if (str.toString().match(pat)) ignore = true;
+  });
+  return ignore;
+};
+
+const getQueryVariable = (variable) => {
+  const query = window.location.search.substring(1);
+  const vars = query.split("&");
+  for (let i = 0; i < vars.length; i += 1) {
+    const pair = vars[i].split("=");
+    if (pair[0] === variable) {
+      return pair[1];
     }
-    return "";
-}
+  }
+  return "";
+};
 
 // validate the user input
-function validateInput() {
-    if ($("#username").val().length > 0 && $("#repository").val().length > 0) {
-        $("#get-stats-button").prop("disabled", false);
-    } else {
-        $("#get-stats-button").prop("disabled", true);
-    }
-}
+const validateInput = () => {
+  if ($("#username").val().length > 0 && $("#repository").val().length > 0) {
+    $("#get-stats-button").prop("disabled", false);
+  } else {
+    $("#get-stats-button").prop("disabled", true);
+  }
+};
 
 // Callback function for getting user repositories
-function getUserRepos() {
-    var user = $("#username").val();
+const getUserRepos = () => {
+  const user = $("#username").val();
 
-    var autoComplete = $('#repository').typeahead();
-    var repoNames = [];
+  const repoNames = [];
 
-    var url = apiRoot + "users/" + user + "/repos";
-    $.getJSON(url, function(data) {
-        $.each(data, function(index, item) {
-            repoNames.push(item.name);
-        });
+  const url = `${apiRoot}users/${user}/repos`;
+  $.getJSON(url, (data) => {
+    $.each(data, (index, item) => {
+      repoNames.push(item.name);
     });
-
-    autoComplete.data('typeahead').source = repoNames;
-}
+  });
+  $("#repository").autocomplete({ source: repoNames });
+};
 
 // Display the stats
-function showStats(data) {
+const showStats = (data) => {
+  let err = false;
+  let errMessage = "";
 
-    var err = false;
-    var errMessage = '';
+  if (data.status === 404) {
+    err = true;
+    errMessage = "The project does not exist!";
+  }
 
-    if(data.status == 404) {
-        err = true;
-        errMessage = "The project does not exist!";
-    }
+  if (data.status === 403) {
+    err = true;
+    errMessage =
+      "You've exceeded GitHub's rate limiting.<br />Please try again in about an hour.";
+  }
 
-    if(data.status == 403) {
-        err = true;
-        errMessage = "You've exceeded GitHub's rate limiting.<br />Please try again in about an hour.";
-    }
+  if (data.length === 0) {
+    err = true;
+    errMessage = "There are no releases for this project";
+  }
 
-    if(data.length == 0) {
-        err = true;
-        errMessage = "There are no releases for this project";
-    }
+  let html = "";
 
-    var html = '';
+  if (err) {
+    html = `<div class='col-md-8 col-md-offset-4 d-inline-block error output'>${errMessage}</div>`;
+    $("#projectHead").show();
+  } else {
+    html += "<div class='col-md-8 offset-md-2 d-block text-left output'>";
+    let latest = true;
+    let totalDownloadCount = 0;
+    let firstRelease = new Date();
 
-    if(err) {
-        html = "<div class='col-md-6 col-md-offset-3 error output'>" + errMessage + "</div>";
-    } else {
-        html += "<div class='col-md-6 col-md-offset-3 output'>";
-        var latest = true;
-        var totalDownloadCount = 0;
+    // Sort by publish date
+    data.sort((a, b) => {
+      return a.published_at < b.published_at ? 1 : -1;
+    });
 
-        // Sort by publish date
-        data.sort(function(a, b) {
-            return (a.published_at < b.published_at) ? 1 : -1;
+    $.each(data, (index, item) => {
+      const releaseTag = item.tag_name;
+      const releaseURL = item.html_url;
+      const releaseAssets = item.assets;
+
+      const hasAssets = releaseAssets.length > 0;
+      const releaseAuthor = item.author;
+      const hasAuthor = releaseAuthor != null;
+
+      const pubDate = new Date(item.published_at);
+      const publishDate = pubDate.toLocaleDateString();
+      firstRelease = firstRelease > pubDate ? pubDate : firstRelease;
+
+      let downloadInfoHTML = "<h6 class='pt-2'>No downloadable assests.</h4>";
+      let ReleaseDownloadCount = 0;
+
+      if (hasAssets) {
+        downloadInfoHTML = `
+                <table class="table table-sm">
+                    <thead>
+                        <tr>
+                        <th scope="col">name</th>
+                        <th scope="col">size</th>
+                        <th scope="col">last update</th>
+                        <th scope="col">d/l</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                    `;
+
+        // desc
+        releaseAssets.sort((a, b) => {
+          return a.download_count > b.download_count ? -1 : 1;
         });
 
-        $.each(data, function(index, item) {
-            var releaseTag = item.tag_name;
-            var releaseURL = item.html_url;
-            var releaseAssets = item.assets;
-            var hasAssets = releaseAssets.length != 0;
-            var releaseAuthor = item.author;
-            var hasAuthor = releaseAuthor != null;
-            var publishDate = item.published_at.split("T")[0];
-			var ReleaseDownloadCount = 0;
+        let goodRows = "";
+        let ignoreRows = "";
 
-            if(latest) {
-                html += "<div class='row release latest-release'>" +
-                    "<h2><a href='" + releaseURL + "' target='_blank'>" +
-                    "<span class='glyphicon glyphicon-tag'></span>&nbsp&nbsp" +
-                    "Latest Release: " + releaseTag +
-                    "</a></h2><hr class='latest-release-hr'>";
-                latest = false;
-            } else {
-                html += "<div class='row release'>" +
-                    "<h4><a href='" + releaseURL + "' target='_blank'>" +
-                    "<span class='glyphicon glyphicon-tag'></span>&nbsp&nbsp" +
-                    releaseTag +
-                    "</a></h4><hr class='release-hr'>";
-            }
+        $.each(releaseAssets, (idx, asset) => {
+          const assetSize = (asset.size / 1048576.0)
+            .toFixed(2)
+            .toLocaleString();
+          const lastUpdate = new Date(asset.updated_at).toLocaleDateString();
 
-            if(hasAssets) {
-                var downloadInfoHTML = "<h4><span class='glyphicon glyphicon-download'></span>" +
-                    "&nbsp&nbspDownload Info: </h4>";
-				downloadInfoHTML += "<ul>";
-                html += "<ul>";
-                $.each(releaseAssets, function(index, asset) {
-                    var assetSize = (asset.size / 1048576.0).toFixed(2).replace(/\./, ',');
-                    var lastUpdate = new Date(asset.updated_at).toLocaleDateString();
-                    downloadInfoHTML += "<li>" + asset.name + " (" + assetSize + " MiB)<br>" +
-                        "<i>Last updated on " + lastUpdate + " — Downloaded " +
-                        asset.download_count.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1&#8239;') + " times</i></li>";
-					totalDownloadCount += asset.download_count;
-					ReleaseDownloadCount += asset.download_count;
-                });
-            }
-			
-            html += "<h4><span class='glyphicon glyphicon-info-sign'></span>&nbsp&nbsp" +
-                "Release Info:</h4>";
+          let rowClass = "row-ignore";
+          if (shouldIgnore(asset.name) === false) {
+            rowClass = "row-good";
+            totalDownloadCount += asset.download_count;
+            ReleaseDownloadCount += asset.download_count;
+          }
 
-            html += "<ul>";
+          const rowHTML = `
+                            <tr class="${rowClass}">
+                                <td>
+                                <span class="${getIconClass(asset)} pr-1"/>
+                                ${asset.name}
+                                </td>
+                                <td>${assetSize} MiB</td>
+                                <td>${lastUpdate}</td>
+                                <td>${asset.download_count.toLocaleString()}</td>
+                            </tr>
+                            `;
 
-            html += "<li><span class='glyphicon glyphicon-calendar'></span>&nbsp&nbspPublished on: " +
-                publishDate + "</li>";
-		
-            if(hasAuthor) {
-                html += "<li><span class='glyphicon glyphicon-user'></span>&nbsp&nbspRelease Author: " +
-                    "<a href='" + releaseAuthor.html_url + "'>" + releaseAuthor.login +"</a><br></li>";
-            }
-
-            html += "<li><span class='glyphicon glyphicon-download'></span>&nbsp&nbspDownloads: " +
-                ReleaseDownloadCount.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1&#8239;') + "</li>";
-
-            html += "</ul>";
-			
-			html += downloadInfoHTML;
-        
-            html += "</div>";
+          if (shouldIgnore(asset.name) === false) {
+            goodRows += rowHTML;
+          } else {
+            ignoreRows += rowHTML;
+          }
         });
 
-        if(totalDownloadCount > 0) {
-            totalDownloadCount = totalDownloadCount.toLocaleString(); //totalDownloadCount.toString().replace(/(\d)(?=(\d{3})+$)/g, '$1&#8239;');
-            var totalHTML = "<div class='row total-downloads'>";
-            totalHTML += "<h2><span class='glyphicon glyphicon-download'></span>" +
-                "&nbsp&nbspTotal Downloads</h2> ";
-            totalHTML += "<span>" + totalDownloadCount + "</span>";
-            totalHTML += "</div>";
-            html = totalHTML + html;
-        }
+        downloadInfoHTML += `${goodRows}${ignoreRows}
+                                     </table>`;
+      }
 
-        html += "</div>";
+      /* release header */
+      let rowClass = "release";
+      if (latest) {
+        rowClass = `${rowClass} latest-release`;
+        latest = false;
+      }
+
+      html += `
+        <div class='row ${rowClass} pt-2'>
+            <div class='col-8 p-0'>
+                <h4 >
+                    <a href='${releaseURL}' target='_blank'>
+                    <span class='fa fa-tag pr-2'></span>${releaseTag}
+                    </a>
+                </h4>
+            </div>
+            <div class='col-4'> 
+                <div class='release-dl-count'>
+                <span class='fa fa-download pr-1'></span>${ReleaseDownloadCount}
+                </div>
+            </div>
+        `;
+
+      /* release info */
+      let author;
+      if (hasAuthor) {
+        author = `by <a href='${releaseAuthor.html_url}'> 
+                                <img class="author-img" src='${releaseAuthor.avatar_url}'/>
+                                ${releaseAuthor.login}
+                            </a>`;
+      }
+      html += `<div class="col-12">
+                        <div class="author border-bottom pb-1 border-secondary">
+                            <span class='fa fa-info-circle'></span> published ${publishDate} ${author}
+                        </div>
+                    </div>
+                    `;
+
+      /* add downloads */
+      html += downloadInfoHTML;
+
+      html += "</div>";
+    });
+
+    if (totalDownloadCount > 0) {
+      totalDownloadCount = totalDownloadCount.toLocaleString();
+      const totalHTML = `
+                <div class='col-md-10 offset-md-1 d-block text-center total-downloads mb-3'>
+                    <h2 class="mr-2">
+                        <span class='fa fa-download pr-2'></span> <span class="">${totalDownloadCount}</span> Downloads
+                    </h2>
+                    since ${firstRelease.toLocaleDateString()} 
+                </div>
+                `;
+      html = totalHTML + html;
     }
 
-    var resultDiv = $("#stats-result");
-    resultDiv.hide();
-    resultDiv.html(html);
-    $("#loader-gif").hide();
-    resultDiv.slideDown();
-}
+    html += "</div>";
+  }
+
+  const lookupDiv = $("#lookupForm");
+  const resultDiv = $("#stats-result");
+  resultDiv.hide();
+  resultDiv.html(html);
+  $("#loader-gif").hide();
+  lookupDiv.hide();
+  resultDiv.slideDown();
+};
 
 // Callback function for getting release stats
-function getStats() {
-    var user = $("#username").val();
-    var repository = $("#repository").val();
+const getStats = () => {
+  const user = $("#username").val();
+  const repository = $("#repository").val();
 
-    var url = apiRoot + "repos/" + user + "/" + repository + "/releases";
-    $.getJSON(url, showStats).fail(showStats);
-}
+  const url = `${apiRoot}repos/${user}/${repository}/releases`;
+  $.getJSON(url, showStats).fail(showStats);
+};
 
 // The main function
-$(function() {
-    $("#loader-gif").hide();
+$(() => {
+  validateInput();
+  $("#username, #repository").keyup(validateInput);
 
-    validateInput();
-    $("#username, #repository").keyup(validateInput);
+  $("#username").change(getUserRepos);
 
-    $("#username").change(getUserRepos);
+  $("#get-stats-button").click(() => {
+    window.location = `?username=${$("#username").val()}&repository=${$(
+      "#repository"
+    ).val()}`;
+  });
 
-    $("#get-stats-button").click(function() {
-        window.location = "?username=" + $("#username").val() +
-            "&repository=" + $("#repository").val();
-    });
-
-    $('#repository').on('keypress',function(e) {
-        if(e.which == 13) {
-            window.location = "?username=" + $("#username").val() +
-            "&repository=" + $("#repository").val();
-        }
-    });
-
-    var username = getQueryVariable("username");
-    var repository = getQueryVariable("repository");
-
-    if(username != "" && repository != "") {
-        $("#username").val(username);
-        $("#repository").val(repository);
-        validateInput();
-        getUserRepos();
-        $(".output").hide();
-        $("#description").hide();
-        $("#loader-gif").show();
-        getStats();
+  $("#repository").on("keypress", (evt) => {
+    if (evt.which === 13) {
+      window.location = `?username=${$("#username").val()}&repository=${$(
+        "#repository"
+      ).val()}`;
     }
+  });
+
+  const username = getQueryVariable("username");
+  const repository = getQueryVariable("repository");
+
+  if (username !== "" && repository !== "") {
+    $("#username").val(username);
+    $("#repository").val(repository);
+    validateInput();
+    getUserRepos();
+    $(".output").hide();
+    $("#projectName").html(repository);
+    $("#projectHead").show();
+    $("#description").hide();
+    $("#loader-gif").show();
+    getStats();
+  } else {
+    $("#lookupForm").show();
+  }
 });
+
+/**
+ * A bunch of stuff to get a silly file icon
+ */
+
+const faIconClasses = {
+  // Media
+  image: "fa fa-file-image-o",
+  audio: "fa fa-file-audio-o",
+  video: "fa fa-file-video-o",
+  // Documents
+  "application/pdf": "fa fa-file-pdf-o",
+  "application/msword": "fa fa-file-word-o",
+  "application/vnd.ms-word": "fa fa-file-word-o",
+  "application/vnd.oasis.opendocument.text": "fa fa-file-word-o",
+  "application/vnd.openxmlformats-officedocument.wordprocessingml":
+    "fa fa-file-word-o",
+  "application/vnd.ms-excel": "fa fa-file-excel-o",
+  "application/vnd.openxmlformats-officedocument.spreadsheetml":
+    "fa fa-file-excel-o",
+  "application/vnd.oasis.opendocument.spreadsheet": "fa fa-file-excel-o",
+  "application/vnd.ms-powerpoint": "fa fa-file-powerpoint-o",
+  "application/vnd.openxmlformats-officedocument.presentationml":
+    "fa fa-file-powerpoint-o",
+  "application/vnd.oasis.opendocument.presentation": "fa fa-file-powerpoint-o",
+  "text/plain": "fa fa-file-text-o",
+  "text/html": "fa fa-file-code-o",
+  "application/json": "fa fa-file-code-o",
+  // Archives
+  "application/gzip": "far fa-file-archive",
+  "application/zip": "far fa-file-archive",
+  "application/x-msdos-program": "fab fa-windows", // fab
+};
+
+function getIconClass(asset) {
+  const mimeType = asset.content_type;
+  if (!mimeType || typeof mimeType !== "string") {
+    return false;
+  }
+
+  if (iconClassesByMime[mimeType]) return iconClassesByMime[mimeType];
+  // check built-in ones...
+  if (faIconClasses[mimeType]) return faIconClasses[mimeType];
+
+  // get the extension and check it
+  const extension = asset.name.split(".").pop().toLowerCase();
+
+  if (!extension) {
+    return false;
+  }
+
+  if (iconClassesByExt[extension]) return iconClassesByExt[extension];
+  console.warn("Unable to find icon: ", asset.name, asset.content_type);
+  return "far fa-file";
+}
